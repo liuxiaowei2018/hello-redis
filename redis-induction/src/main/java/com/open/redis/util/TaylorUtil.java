64a0ffa57e9685eb02ffef1c946d07d5926228b9
刘校伟 <liuxiaowei@pharmakeyring.com>
2022-11-04 15:36:00 +0800

    style: 二进制随机数生成工具

---
+++ redis-induction/src/main/java/com/open/redis/util/TaylorUtil.java
@@ -0,0 +1,49 @@
+package com.open.redis.util;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author liuxiaowei
+ * @date 2022年11月03日 20:49
+ * @Description
+ * 二进制使用率: max_diff/(max-min)=85899345919/89999999999=95.4%
+ */
+@Slf4j
+public class TaylorUtil {
+
+    private static final long MIN = 10000000000L;
+
+    // 11位后缀ID(17179869183)
+    private static final long SUFFIX_ID = ((1L << 34) - 1);
+    // 12位前缀ID(120259084288)
+    private static final long PRE_ID = (7L << 34);
+    // 最大ID(11位)
+    private static final long MAX_ID = 85899345919L;
+
+
+    public static long next(long id) {
+
+        if (id < 0 || id > MAX_ID) {
+            throw new IllegalArgumentException("Illegal id: " + id);
+        }
+
+        // 非线性
+        id = swap(id, 7, 0);
+        id = swap(id, 6, 1);
+
+        // Id后34位
+        long value = id & SUFFIX_ID;
+
+        // Id后34位的最低八位和高1-9位交换
+        value = (((value >> 8) | ((value & 255) << 26)));
+        // Id前3位|Id后34位
+        id = (id & PRE_ID) | value;
+        // 加上最小值, 保证生成的11位
+        id += MIN;
+        return id;
+    }
+
+    private static long swap(long value, int x, int y) {
+        return value & (~(1 << x)) & (~(1 << y)) | ((value >> x) & 1) << y | ((value >> y) & 1) << x;
+    }
+}
--- redis-induction/src/test/java/com/open/redis/ApplicationTest.java
+++ redis-induction/src/test/java/com/open/redis/ApplicationTest.java
@@ -1,27 +1,85 @@
 package com.open.redis;
 
+import cn.hutool.core.convert.Convert;
+import com.open.redis.util.TaylorUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * @author liuxiaowei
  * @date 2022年09月14日 16:01
  * @Description
  */
+@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTest {
 
     @Autowired
-    private StringRedisTemplate stringRedisTemplate;
+    private RedisTemplate<String,Object> redisTemplate;
 
     @Test
-    public void testStringSetKey() {
-        stringRedisTemplate.opsForValue().set("demo", "like");
+    public void testId() {
+        Set objects = new HashSet<>();
+        long sum = 0L;
+        for (int i = 0; i < 1000; i++) {
+            List<Long> id2022 = getResult("id20221103", 1000);
+            for (Long s : id2022) {
+                log.info(String.valueOf(s));
+                sum++;
+                if (objects.contains(id2022)) {
+                   log.error("重复---{}",s);
+                }
+                objects.add(s);
+            }
+        }
+        log.info("操作次数---:{}",sum);
     }
+
+    private List<Long> getResult(String key, Integer num) {
+        Object obj = redisTemplate.opsForValue().get(key);
+        Long memoryIncr;
+        if (obj == null) {
+            memoryIncr = 10000000000L;
+        } else {
+            if (obj instanceof CharSequence) {
+                memoryIncr = Long.parseLong(((String) obj));
+            } else {
+                memoryIncr = Convert.toLong(obj);
+            }
+        }
+        Long increment = redisTemplate.opsForValue().increment(key, num == null ? 1 : num);
+        ArrayList<Long> codes = new ArrayList<>();
+        for (; memoryIncr < increment; ) {
+            codes.add(TaylorUtil.next(memoryIncr));
+            memoryIncr++;
+        }
+        return codes;
+    }
+
+    public static void main(String[] args){
+        Set set = new HashSet();
+        for(int i=0;; i++) {
+            long id = TaylorUtil.next(i);
+            System.out.println(id +"---"+i);
+            if(set.contains(id)){
+                System.out.println("duplicated");
+                break;
+            }
+            set.add(id);
+        }
+    }
+
+
 
 }
